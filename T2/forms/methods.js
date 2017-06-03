cf1da8c88065d220dfe1b913613cc7c6ad813fdd
Henrique1792 <freitash1792@gmail.com>
2017-06-03 13:39:06 -0300

    Creating database, but I dunno why cannot insert :C

--- T2/forms/methods.js
+++ T2/forms/methods.js
@@ -1,58 +1,56 @@
-function LoadDb(){
-    var request= indexedDB.open("Teste");
+$(document).ready(function(){
+    var request=indexedDB.open('Pets',1);
 
-    request.onerror=function(event){
-        alert("não habilitou o indexedDb, seu jagunço!");
-    };
-    request.onsuccess=function(event){
-        var db=event.target.result;
-        var objectStore=db.createObjectStore("Animals",{keyPath: "ID"});
-        var transaction=db.transaction(["Animals"], "versionchange");
-        
-        transaction.onsuccess=function(event){
-            console.log("Db created");
-            objectStore.createIndex("ID", "ID", {unique: false});
-            objectStore.transaction.oncomplete=function(event){
-                console.log("ready to receive data;");
-            }
-        
+    request.onsuccess=function(e){
+        console.log("Success creating database!");
+        var db=e.target.result;
+    
+    }
+
+    request.onerror=function(){
+        console.log("Messed up on creating database!");
+    
+    }
+
+
+    request.onupgradeneeded=function(e){
+        var db=e.target.result;
+        if(!db.objectStoreNames.contains('Animals')){
+            var os=db.createObjectStore('Animals',{keyPath: "ID",autoIncrement: true});
+
         }
         
-        
     }
-}   
+
+});
 
 
-function getAnimal(){
-       if("indexedDb" in window){
-            var k=document.getElementById("pet-form");
-            var db=indexedDB.open("Teste");
-            var request=db.transaction(["Animals"], "readwrite").objectStore("Animals").add({name:k.item[0],ID: k.item[1], raca:k.item[2],idade:k.item[3],image:data.item[4]});
-            request.onsucces=function(event){
-                alert("YAY IT WORKED, BITCH!");
-            
-            };
-            request.onerror=function(event){
-                alert("Uh-oh...");
-            }
-            
-       }
-        
-        
-        
-    };
+function addPet(){
+    var petname=$('petname').val();
+    var raca=$('raca').val();
+    var idade=$('idade').val();
+    var image=$('image').val();
+    
+    var transaction = db.transaction(["Animals"], "readwrite");
+      
+    var store=transaction.objectStore("Animals");
 
+    var animal={
+        name: name,
+        raca: raca,
+        idade: idade,
+        image: image
+    }
 
-function getService(){
-    if("indexedDb" in window){
-        const form=document.getElementById("service-form");
+    var request = store.add(animal);
+    
+    request.onsuccess=function(e){
+        alert("Pet Added");
     }
     
-}
-
-function getProduct(){
-    if("indexedDb" in window){
-        const form=document.getElementById("product-form");
-    }    
+    request.onerror=function(){
+        alert("Sorry, cannot add your pet at the time");
+        conslole.log("Error",e.target.error.target.name);
     
+    }
 }
