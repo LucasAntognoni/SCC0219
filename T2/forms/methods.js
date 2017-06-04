9c3149baacf3ec2049413450a5cdaf19c76ceb1b
Henrique1792 <freitash1792@gmail.com>
2017-06-04 05:28:46 -0300

    call to add working, but still not adding, help please .-.

--- T2/forms/methods.js
+++ T2/forms/methods.js
@@ -1,29 +1,32 @@
+'use strict';
+
 let db;
 
 $(document).ready(function(){
     var request=indexedDB.open('Pets',1);
 
     request.onsuccess=function(e){
-        console.log("Success creating database!");
-        db=e.target.result;
-    }
+       db=e.target.result;
+       console.log("Database's up!");
+    };
 
     request.onerror=function(){
         console.log("Messed up on creating database!");
     
-    }
-
+    };
 
     request.onupgradeneeded=function(e){
        db=e.target.result;
-       if(!db.objectStore.names.contains("Animals")){
+       if(!db.objectStoreNames.contains("Animals")){
             var os=db.createObjectStore("Animals",{keyPath: "ID", autoIncrement: true});
             os.createIndex('petname','petname',{unique:false});
             os.createIndex('pedigree', 'pedigree',{unique:false});
             os.createIndex('age','age',{unique:false});
             os.createIndex('image','image',{unique:false});
        }   
-    }
+    };
+
+
 
 });
 
@@ -39,14 +42,8 @@ function addPet(){
         pedigree: raca,
         age: idade,
         image: image
-    };
-   var transaction=db.transaction(["Animals"],"readwrite");
-   transaction.oncomplete=function(e){
-       console.log("Transaction stablished. Adding Pet");
-   };
-   transaction.onerror=function(e){
-       console.log("Transaction messed up. Please refresh your system");
-   };
+    }
+     var transaction=db.transaction(["Animals"],"readwrite");
     
     var os=transaction.objectStore("Animals");
 
@@ -57,6 +54,8 @@ function addPet(){
     };
     
     request.onerror=function(e){
-        console.log("I'm sorry Dave, I'm afraid I cannot do that");
+        console.log("I'm sorry Dave, I'm afraid I cannot do that", e.target.error.name);
     };
+   
 }
+
