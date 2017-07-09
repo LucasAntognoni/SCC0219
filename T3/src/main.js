0da3e3a2f1109196857cec13f5f9dfc5921076a1
cstenico <k.stenico@gmail.com>
2017-07-09 17:29:10 -0300

    Organizando diretorio

--- T3/src/main.js
+++ T3/src/main.js
@@ -5,7 +5,7 @@ const path = require('path');
 const nano = require('nano')('http://localhost:5984');
 const app = express();
 
-app.use(express.static(__dirname +'/view/public'));
+app.use(express.static(__dirname +'/website/public'));
 app.use(express.static(__dirname+'/website'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,46 +24,56 @@ app.use((err, request, response, next) => {
     next();
 });
 
+app.use('script', express.static(__dirname + '/website/js/'));
+
 app.get('/', (request, response) => {
-    response.sendFile(path.join(__dirname+'/view/index.html'));
+    response.sendFile(path.join(__dirname+'index.html'));
+});
+
+app.get('/index.html', (request, response) => {
+    response.sendFile(path.join(__dirname+'/index.html'));
 });
 
 app.get('/about.html', (request, response) => {
-    response.sendFile(path.join(__dirname+'/view/about.html'));
+    response.sendFile(path.join(__dirname+'/about.html'));
 });
 
 app.get('/login_cadastro/login.html', (request, response) => {
-    response.sendFile(path.join(__dirname+'/view/login_cadastro/login.html'));
+    response.sendFile(path.join(__dirname+'/login_cadastro/login.html'));
 });
 app.get('/login_cadastro/cadastro.html', (request, response) => {
-    response.sendFile(path.join(__dirname+'/view/login_cadastro/cadastro.html'));
+    response.sendFile(path.join(__dirname+'/login_cadastro/cadastro.html'));
 });
 app.get('/ganhos/ganhos.html', (request, response) => {
-    response.sendFile(path.join(__dirname+'/view/ganhos/ganhos.html'));
+    response.sendFile(path.join(__dirname+'/ganhos/ganhos.html'));
 });
 app.get('/vendas/vendaProdutos.html', (request, response) => {
-    response.sendFile(path.join(__dirname+'/view/vendas/vendaProdutos.html'));
+    response.sendFile(path.join(__dirname+'/vendas/vendaProdutos.html'));
 });
 app.get('/vendas/vendaServicos.html', (request, response) => {
-    response.sendFile(path.join(__dirname+'/view/vendas/vendaServicos.html'));
+    response.sendFile(path.join(__dirname+'/vendas/vendaServicos.html'));
 });
 app.get('/gerenciamento/produtos.html', (request, response) => {
-    response.sendFile(path.join(__dirname+'/view/gerenciamento/produtos.html'));
+    response.sendFile(path.join(__dirname+'/gerenciamento/produtos.html'));
 });
 app.get('/gerenciamento/servicos.html', (request, response) => {
-    response.sendFile(path.join(__dirname+'/view/gerenciamento/servicos.html'));
+    response.sendFile(path.join(__dirname+'/gerenciamento/servicos.html'));
 });
 app.get('/agendamento/agendamento.html', (request, response) => {
-    response.sendFile(path.join(__dirname+'/view/agendamento/agendamento.html'));
+    response.sendFile(path.join(__dirname+'/agendamento/agendamento.html'));
 });
 app.get('/forms/animals.html', (request, response) => {
-    response.sendFile(path.join(__dirname+'/view/forms/animals.html'));
+    response.sendFile(path.join(__dirname+'/forms/animals.html'));
 });
 app.get('/forms/prods.html', (request, response) => {
-    response.sendFile(path.join(__dirname+'/view/forms/prods.html'));
+    response.sendFile(path.join(__dirname+'/forms/prods.html'));
 });
 app.get('/forms/servs.html', (request, response) => {
-    response.sendFile(path.join(__dirname+'/view/forms/servs.html'));
+    response.sendFile(path.join(__dirname+'/forms/servs.html'));
+});
+
+app.get('/carrinho/checkout.html', (request, response) => {
+    response.sendFile(path.join(__dirname+'/carrinho/checkout.html'));
 });
 
 app.post('/addUser', (request, response) => {
